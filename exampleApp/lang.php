3a5cae1c21a20a82a6ec9918459ba0c145279192
Mjcs95 <mcamberosanchez@gmail.com>
2018-03-20 17:48:05 +0100

    hora corregiDA

--- exampleApp/lang.php
+++ exampleApp/lang.php
@@ -69,7 +69,7 @@ $string['Head_trad4_Credits'] = '(C)".strftime("%Y")." María José Cambero Sán
 $string['Head_trad4_FlagURL'] = 'http://upload.wikimedia.org/wikipedia/commons/2/2d/Animated-Flag-United-Kingdom.gif';
 $string['trad4_ejemploUno'] = 'Next ".strftime ("%A, %B, %Y")." I will go to a conference in New York.';
 $string['trad4_ejemploDos'] = 'My mother thinks that ".number_format($a->amount,7)." euros for this trainers is too expensive.';
-$string['trad4_ejemploTres'] = 'Every Monday at ".strftime("%H, %M").",  we have English classes.';
+$string['trad4_ejemploTres'] = 'Every Monday at ".strftime("%H:%M").",  we have English classes.';
 
 $string['trad5_Lang'] = 'en_GB.utf8';
 $string['trad5_Greeting'] =	'Welcome to the work of the translator $a->numTrad published at $a->time';
