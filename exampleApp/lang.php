f928249115f64e30b40f49bf704270d02a91443b
Cristina G <cristina.gar.aguilar@gmail.com>
2018-03-20 17:21:17 +0100

    CGA 20/03 2

--- exampleApp/lang.php
+++ exampleApp/lang.php
@@ -53,7 +53,7 @@ $string['trad3_NumString'] = 'Completing a league sticker collection costs $a->a
 $string['trad3_CurrencyString'] = 'Completing a league sticker collection costs  ".number_format($a->amount,2)." euros.';
 $string['trad3_Message'] = 'Mr. $a->user_surname has gotten it back even if it could have been done by anyone. ¡Hurra $a->user_name $a->user_surname!';
 $string['Head_trad3_Credits'] = '(C)".strftime("%Y")."Cristina García Aguilar';
-$string['Head_trad3_FlagURL'] = '<a href="http://www.gifsanimados.org/cat-bandera-de-gran-bretana-771.htm"><img src="http://www.gifsanimados.org/data/media/771/bandera-de-gran-bretana-imagen-animada-0026.gif" border="0" alt="bandera-de-gran-bretana-imagen-animada-0026" /></a>';
+$string['Head_trad3_FlagURL'] = 'http://www.gifsanimados.org/data/media/771/bandera-de-gran-bretana-imagen-animada-0026.gif';
 $string['trad3_ejemploUno'] = 'Mr. $a->user_surname has an important meeting on ".strftime("%m/%d/%Y")." .';
 $string['trad3_ejemploDos'] = 'He earns $a->amount €';
 $string['trad3_ejemploTres'] = 'I will see him $a->time';
